708700e9c859b5cca9ab768045d15cd3b1dc45df
Adskiyunskill <64008131+Adskiyunskill@users.noreply.github.com>
2022-02-18 12:10:28 +0300

    <Request>

---
+++ Treug3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Treug3
+{
+    internal class Treug3
+    {
+        
+        static void Main(string[] args)
+        {  {
+                 int A = Convert.ToInt32 (args[0]);
+                 int B = Convert.ToInt32(args[1]);
+                if (A.ToString().Length > B.ToString().Length)
+                {
+                    Console.WriteLine("Число A > B ");
+                }
+                else
+                {
+                    Console.WriteLine("Число B > A ");
+                }
+            }
+        }
+    }
+}
