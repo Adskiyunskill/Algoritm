2bb3be7ffcd16bda737299701070994a933b00b9
Adskiyunskill <64008131+Adskiyunskill@users.noreply.github.com>
2022-02-18 11:21:38 +0300

    <Request>

---
+++ Treug2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Treug2
+{
+    internal class Treug2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AB = " + args[0]);
+            double AB = Convert.ToDouble(args[0]);
+            Console.WriteLine("AC = " + args[1]);
+            double AC = Convert.ToDouble(args[1]);
+            Console.WriteLine("CD = " + args[2]);
+            double DC = Convert.ToDouble(args[2]);
+            double P = AB + AC + DC;
+            Console.WriteLine("P = " + P);
+        }
+    }
+}
+
